d97940a4f61639265ba9cb5eea7967a1a8164444
Ryan Mulligan <ryan@ryantm.com>
2014-04-23 22:55:52 -0700

    work on prime field inverse that doesn't compile

--- src/ecdsa/prime_field.rs
+++ src/ecdsa/prime_field.rs
@@ -1,5 +1,7 @@
 extern crate num;
 use num::bigint::BigUint;
+use num::bigint::ToBigUint;
+use num::bigint::{Zero,Integer};
 
 struct PrimeField {
     prime: BigUint
@@ -11,7 +13,32 @@ impl PrimeField {
         integer < self.prime
     }
 
-    fn mod_floor(&self, integer: BigUint) -> BigUint {
-        integer.mod_floor(self)
+    fn modulo(&self, integer: BigUint) -> BigUint {
+        
+        //num::mod_floor(self.prime, integer)
+        integer.mod_floor(self.prime)
+    }
+
+    fn inverse(&self, n: BigUint) -> BigUint {
+        assert!(!n.is_zero(), "0 has no multiplicative inverse.");
+
+        let t : BigUint  = Zero::zero();
+        let newt : BigUint  = Zero::zero();
+        let r = n;
+        let newr = self.prime;
+
+        while !newr.is_zero() {
+            let quotient = r / newr;
+            let temp = t;
+            let t = newt;
+            let newt = temp - quotient * newt;
+
+            let temp = r;
+            let r = newt;
+            let newr = temp - quotient * newr;
+        }
+        if r > 1u.to_biguint().unwrap() { fail!("prime is not invertible") }
+        if r < Zero::zero() { let t = t + n; }
+        return t;
     }
 }
