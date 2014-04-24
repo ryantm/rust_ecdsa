51882dd5e899d665b4346c8052919ed928cfea87
Ryan Mulligan <ryan@ryantm.com>
2014-04-24 09:05:21 -0700

    make prime_field inverse compile

--- src/ecdsa/prime_field.rs
+++ src/ecdsa/prime_field.rs
@@ -1,7 +1,9 @@
 extern crate num;
 use num::bigint::BigUint;
 use num::bigint::ToBigUint;
-use num::bigint::{Zero,Integer};
+//use num::bigint::{Zero};
+use std::num::Zero;
+use num::Integer;
 
 struct PrimeField {
     prime: BigUint
@@ -16,29 +18,29 @@ impl PrimeField {
     fn modulo(&self, integer: BigUint) -> BigUint {
         
         //num::mod_floor(self.prime, integer)
-        integer.mod_floor(self.prime)
+        integer.mod_floor(&self.prime)
     }
 
     fn inverse(&self, n: BigUint) -> BigUint {
         assert!(!n.is_zero(), "0 has no multiplicative inverse.");
 
-        let t : BigUint  = Zero::zero();
-        let newt : BigUint  = Zero::zero();
-        let r = n;
-        let newr = self.prime;
+        let ref t : BigUint  = Zero::zero();
+        let ref newt : BigUint  = Zero::zero();
+        let ref r = n;
+        let ref newr = self.prime;
 
         while !newr.is_zero() {
-            let quotient = r / newr;
+            let quotient = r / *newr;
             let temp = t;
             let t = newt;
-            let newt = temp - quotient * newt;
+            let newt = temp - quotient * *newt;
 
             let temp = r;
             let r = newt;
-            let newr = temp - quotient * newr;
+            let newr = temp - quotient * *newr;
         }
-        if r > 1u.to_biguint().unwrap() { fail!("prime is not invertible") }
-        if r < Zero::zero() { let t = t + n; }
-        return t;
+        if *r > 1u.to_biguint().unwrap() { fail!("prime is not invertible") }
+        if *r < Zero::zero() { let t = t + n; }
+        return t.clone();
     }
 }
